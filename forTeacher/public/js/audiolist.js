b1430d49108a6d4fbd40c04b6acf3867ea6db230
WangShuai <wangshuai@huangzhongdalv.com>
2018-03-22 12:00:18 +0800

    0322all

---
+++ forTeacher/public/js/audiolist.js
@@ -0,0 +1,35 @@
+import wav1 from '../audio/1.wav'
+import wav2 from '../audio/2.wav'
+import wav3 from '../audio/3.wav'
+import wav4 from '../audio/4.wav'
+import wav5 from '../audio/5.wav'
+import wav6 from '../audio/6.wav'
+import wav7 from '../audio/7.wav'
+import wav8 from '../audio/8.wav'
+import wav9 from '../audio/9.wav'
+import wav10 from '../audio/10.wav'
+import wav11 from '../audio/11.wav'
+import wav12 from '../audio/12.wav'
+import wav13 from '../audio/13.wav'
+import wav14 from '../audio/14.wav'
+import wav15 from '../audio/15.wav'
+import wav16 from '../audio/16.wav'
+import wav17 from '../audio/17.wav'
+import wav18 from '../audio/18.wav'
+import wav19 from '../audio/19.wav'
+import wav20 from '../audio/20.wav'
+import wav21 from '../audio/21.wav'
+import wav22 from '../audio/22.wav'
+
+module.exports = {
+    audio1: wav1,
+    audio2: wav2,
+    audio3: wav3,
+    audio4: wav4,
+    audio5: wav5,
+    audio6: wav6,
+    audio7: wav7,
+    audio8: wav8,
+    audio9: wav9,
+    audio10: wav10
+}
--- forTeacher/public/js/question.js
+++ forTeacher/public/js/question.js
@@ -1,6 +1,10 @@
 !function (e, t) { function n() { var n = l.getBoundingClientRect().width; t = t || 540, n > t && (n = t); var i = 100 * n / e; r.innerHTML = "html{font-size:" + i + "px;}" } var i, d = document, o = window, l = d.documentElement, r = document.createElement("style"); if (l.firstElementChild) l.firstElementChild.appendChild(r); else { var a = d.createElement("div"); a.appendChild(r), d.write(a.innerHTML), a = null } n(), o.addEventListener("resize", function () { clearTimeout(i), i = setTimeout(n, 300), d }, !1), o.addEventListener("pageshow", function (e) { e.persisted && (clearTimeout(i), i = setTimeout(n, 300)) }, !1), "complete" === d.readyState ? d.body.style.fontSize = "16px" : d.addEventListener("DOMContentLoaded", function (e) { d.body.style.fontSize = "16px" }, !1) }(750, 1920);
 
 
+import $ from 'jquery'
+// import audio from 'audiojs'
+import audioList from './audiolist';
+
 var data = [
     {
         question: "请选择正确答案:",
@@ -58,7 +62,38 @@ var data = [
     }
 ];
 
-(function() {
-    let cont = '';
-    cont += ''
-})()
+$(document).on('click', '#showStuScore', function () {
+    // $('#popup').show();
+    if($('#popup').hasClass('popup-hide')){
+        $('#popup').removeClass('popup-hide').addClass('fadeInDown animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
+            $(this).removeClass('fadeInDown animated');
+        });
+    }else{
+        $('#popup').addClass('fadeOutDown animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
+            $(this).removeClass('fadeOutDown animated');
+            $('#popup').addClass('popup-hide');
+        });
+    }
+})
+
+let qaAudio = document.createElement('audio');
+qaAudio.src = audioList.audio3;
+qaAudio.addEventListener('canplaythrough', function(){
+    console.log('success');
+    $('#playAudio').removeAttr('disabled');
+}, false);
+qaAudio.addEventListener('ended', function(){
+    $('#playAudio').text('重新播放');
+})
+// qaAudio.play();
+
+$(document).on('click', '#playAudio', function() {
+    console.log(qaAudio.paused);
+    if(qaAudio.paused) {
+        qaAudio.play();
+        $(this).text('暂停播放');
+    }else{
+        qaAudio.pause();
+        $(this).text('继续播放');
+    }
+})
